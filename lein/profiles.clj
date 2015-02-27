d592e3e0acddba14bfe250f3c8163a184bb4fe03
Evsyukov Denis <denis.evsyukov@gmail.com>
2015-02-27 16:12:42 +0400

    Update profiles.clj

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [lein-bikeshed "0.2.0"]
                   [lein-cljfmt "0.1.4"]
+                  [lein-ancient "0.6.3"]
                   [jonase/eastwood "0.2.1"]
                   [cider/cider-nrepl "0.8.2"]
                   [venantius/ultra "0.2.0"]]
