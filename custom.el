4e76bc5fe0136cd5d8b030c223f0733896a05bb9
Evsyukov Denis <denis@evsyukov.org>
2015-05-11 14:30:27 +0400

    Remove org-config, simplify config file

---
+++ custom.el
@@ -0,0 +1,85 @@
+(setq  user-full-name "Denis Evsyukov"
+       user-mail-address "denis@evsyukov.org")
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+(setq auto-compile-display-buffer nil)
+(setq auto-compile-mode-line-counter t)
+
+(load "~/.emacs.secrets" t)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+
+(setq inhibit-splash-screen t
+      inhbit-startup-message t
+      initial-scratch-message ""
+      inhibit-startup-echo-area-message t)
+
+(when window-system
+  (set-frame-size (selected-frame) 170 50)
+  (set-default-font "Consolas 13" nil t))
+
+(blink-cursor-mode -1)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq visible-bell nil)
+(setq ring-bell-function 'ignore)
+
+(prefer-coding-system 'utf-8)
+(when (display-graphic-p)
+  (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)))
+(setq-default buffer-file-coding-system 'utf-8-unix)
+
+(setq default-input-method "russian-computer")
+
+;;(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+
+;; key bindings
+(when (eq system-type 'darwin) ;; mac specific settings
+  (setq mac-option-modifier 'alt)
+  (setq mac-command-modifier 'meta)
+  (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
+  (define-key global-map [home] 'beginning-of-line)
+  (define-key global-map [end] 'end-of-line))
+
+(setq-default indent-tabs-mode nil)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(setq-default tab-width 2)
+(global-set-key (kbd "RET") 'newline-and-indent)
+(column-number-mode 1)
+
+(use-package ido-vertical-mode
+  :ensure t
+  :init
+  (progn
+    (ido-mode t)
+    (ido-vertical-mode 1)
+    (setq ido-ignore-buffers '("^ " "*Completions*" "*Shell Command Output*"
+                               "*Messages*" "Async Shell Command"))
+    (setq ido-enable-flex-matching t
+          ido-use-virtual-buffers t)))
+
+(use-package exec-path-from-shell
+  :ensure t
+  :init
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize)))
+
+(use-package irony-mode
+  :ensure irony
+  :init
+  (progn
+    (add-hook 'c++-mode-hook 'irony-mode)
+    (add-hook 'c-mode-hook 'irony-mode)
+    (add-hook 'objc-mode-hook 'irony-mode)))
+
--- emacs
+++ emacs
@@ -3,11 +3,27 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
-
+ 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
+ 
 (setq package-enable-at-startup nil)
-(org-babel-load-file "~/.custom.org")
+ 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (irony-mode cedit company ido-vertical-mode use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+ 
+(load "~/.custom.el" t)
