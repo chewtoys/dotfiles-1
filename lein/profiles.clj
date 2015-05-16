d0452a409a77199ed4d762c81de18cf94f946269
Evsyukov Denis <denis@evsyukov.org>
2015-05-16 12:10:53 +0400

    Remove utra, because cider-nrepl wrong working with this plugin

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,1 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]
-                  [venantius/ultra "0.2.0"]]        
-        :ultra {:color-scheme :solarized_dark}        
-        }}
+{:user {:plugins [[cider/cider-nrepl "0.8.2"]]}}
