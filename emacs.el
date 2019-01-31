3698364c37145b539ccb458b57232df451255e24
Evsyukov Denis Anatolyevich <d.evsyukov@tinkoff.ru>
2019-01-31 16:43:25 +0300

    Added ivy package

--- emacs.el
+++ emacs.el
@@ -75,6 +75,8 @@
 
 (blink-cursor-mode -1)
 
+(electric-pair-mode t)
+
 (fset 'yes-or-no-p 'y-or-n-p)
 
 ;; (global-prettify-symbols-mode t)
@@ -406,3 +408,34 @@
   (global-set-key (kbd "C-c u") 'clang-format-buffer)
 
   (setq clang-format-style-option "llvm"))
+
+(use-package ivy
+  :ensure t
+  :init
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  (global-set-key (kbd "<f6>") 'ivy-resume))
+
+(use-package swiper
+  :ensure t
+  :init
+  (global-set-key "\C-s" 'swiper))
+
+(use-package counsel
+  :ensure t
+  :init
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  (global-set-key (kbd "C-c g") 'counsel-git)
+  (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c k") 'counsel-ag)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
