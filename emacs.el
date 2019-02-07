4a1a1a118b9f415f337dd3b4bf4e8ab703b69d79
Evsyukov Denis Anatolyevich <d.evsyukov@tinkoff.ru>
2019-02-07 09:37:09 +0300

    Change configuration
    Do not use ivy
    Change powerline theme

--- emacs.el
+++ emacs.el
@@ -293,7 +293,7 @@
 
 (use-package projectile
   :ensure t
-  :diminish projectile-mode
+  ;; :diminish projectile-mode
   :config
   (projectile-global-mode))
 
@@ -385,7 +385,7 @@
 (use-package powerline
   :ensure t
   :config
-  (powerline-default-theme))
+  (powerline-vim-theme))
 
 (use-package git-gutter
   :ensure t
@@ -401,40 +401,9 @@
 
   (setq clang-format-style-option "llvm"))
 
-(use-package ivy
-  :ensure t
-  :init
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (setq enable-recursive-minibuffers t)
-  (global-set-key (kbd "C-c C-r") 'ivy-resume)
-  (global-set-key (kbd "<f6>") 'ivy-resume))
-
-(use-package swiper
-  :ensure t
-  :init
-  (global-set-key "\C-s" 'swiper))
-
-(use-package counsel
-  :ensure t
-  :init
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-  (global-set-key (kbd "<f1> l") 'counsel-find-library)
-  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-  (global-set-key (kbd "C-c g") 'counsel-git)
-  (global-set-key (kbd "C-c j") 'counsel-git-grep)
-  (global-set-key (kbd "C-c k") 'counsel-ag)
-  (global-set-key (kbd "C-x l") 'counsel-locate)
-  (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
-
 (use-package yasnippet
   :ensure t
-  :diminish yas-minor-mode
+  ;; :diminish yas-minor-mode
   :bind (:map yas-minor-mode-map
               ("C-x i i" . yas-insert-snippet)
               ("C-x i n" . yas-new-snippet)
