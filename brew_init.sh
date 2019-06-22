87ef96fbb86c0988e89727f5c7aaa5633ad35b72
Denis Evsyukov <d.evsyukov@MacBook-Pro-Denis.local>
2019-06-22 10:35:43 +0300

    Added hunspell

--- brew_init.sh
+++ brew_init.sh
@@ -6,7 +6,7 @@ if ! brew ls --versions bash > /dev/null; then
 fi
 
 echo "Installing sofware"
-brew install bash bash-completion@2 bat coreutils fortune git go haskell-stack pandoc rbenv pyenv ripgrep rlwrap rustup-init the_silver_searcher tmux tree upx wget xz z
+brew install bash bash-completion@2 bat coreutils fortune git go haskell-stack hunspell pandoc rbenv pyenv ripgrep rlwrap rustup-init the_silver_searcher tmux tree upx wget xz z
 
 echo "Installing go tools..."
 if ! type "goimports" > /dev/null; then
