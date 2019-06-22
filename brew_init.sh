d80def4c047fba178b38ed26a0f493843745b095
Denis Evsyukov <d.evsyukov@MacBook-Pro-Denis.local>
2019-06-22 08:43:54 +0300

    Update

--- brew_init.sh
+++ brew_init.sh
@@ -6,7 +6,9 @@ if ! brew ls --versions bash > /dev/null; then
 fi
 
 echo "Installing sofware"
-brew install cask bash bash-completion@2 bat coreutils fortune git go haskell-stack pandoc rbenv pyenv ripgrep rlwrap rustup-init the_silver_searcher tmux tree upx wget xz z
+brew install bash bash-completion@2 bat coreutils fortune git go haskell-stack pandoc rbenv pyenv ripgrep rlwrap rustup-init the_silver_searcher tmux tree upx wget xz z
 
 echo "Installing go tools..."
-go get -u golang.org/x/tools/...
+if ! type "goimports" > /dev/null; then
+    go get -u golang.org/x/tools/...
+fi
