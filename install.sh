9b4b132357e06a83a13a3dd10f36a572824268ac
Denis Evsyukov <denis@evsyukov.org>
2019-08-10 13:36:26 +0300

    Fixes

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+# set -x
 
-ln -sf $(pwd)/bin ~/bin
+ln -sf $(pwd)/bin_files ~/bin
 ln -sf $(pwd)/dircolors ~/.dircolors
 ln -sf $(pwd)/gemrc ~/.gemrc
 ln -sf $(pwd)/gitconfig ~/.gitconfig
@@ -10,6 +11,8 @@ ln -sf $(pwd)/tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/colordiffrc ~/.colordiffrc
 ln -sf $(pwd)/ediff-merge-script /usr/local/bin/ediff-merge-script
 ln -sf $(pwd)/aliases ~/.aliases
-ln -sf $(pwd)/bash ~/.bash
+ln -sf $(pwd)/bash_files ~/.bash
 ln -sf $(pwd)/bashrc ~/.bashrc
 ln -sf $(pwd)/bash_profile ~/.bash_profile
+
+cp -r $(pwd)/stack ~/.stack
