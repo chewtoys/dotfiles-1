a55ff843753a3ebd5718ad14f5d5e9d88d60b4c3
Denis Evsyukov <denis@evsyukov.org>
2019-08-10 13:07:54 +0300

    Added bin to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ln -sf $(pwd)/bin ~/bin
 ln -sf $(pwd)/dircolors ~/.dircolors
 ln -sf $(pwd)/gemrc ~/.gemrc
 ln -sf $(pwd)/gitconfig ~/.gitconfig
@@ -8,7 +9,7 @@ ln -sf $(pwd)/inputrc ~/.inputrc
 ln -sf $(pwd)/tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/colordiffrc ~/.colordiffrc
 ln -sf $(pwd)/ediff-merge-script /usr/local/bin/ediff-merge-script
-ln -sf $(pwd)/aliases  ~/.aliases
-ln -sf $(pwd)/bash  ~/.bash
-ln -sf $(pwd)/bashrc  ~/.bashrc
-ln -sf $(pwd)/bash_profile  ~/.bash_profile
+ln -sf $(pwd)/aliases ~/.aliases
+ln -sf $(pwd)/bash ~/.bash
+ln -sf $(pwd)/bashrc ~/.bashrc
+ln -sf $(pwd)/bash_profile ~/.bash_profile
