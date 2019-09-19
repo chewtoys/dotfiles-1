79c70bb426e4b3d22cd605606d4fb242b2760c42
Evsyukov Denis Anatolyevich <d.evsyukov@tinkoff.ru>
2019-09-19 11:15:57 +0300

    Install nvim

--- brew_init.sh
+++ brew_init.sh
@@ -6,7 +6,7 @@ if ! brew ls --versions bash > /dev/null; then
 fi
 
 echo "Installing sofware"
-brew install autojump bash bash-completion@2 bat coreutils fortune git go hunspell pandoc rbenv roswell pyenv ripgrep rlwrap rustup-init the_silver_searcher tmux tree upx wget xz
+brew install autojump bash bash-completion@2 bat coreutils fortune git go hunspell pandoc rbenv roswell pyenv ripgrep rlwrap rustup-init the_silver_searcher tmux tree upx wget xz nvim
 
 echo "Installing go tools..."
 if ! type "goimports" > /dev/null; then
